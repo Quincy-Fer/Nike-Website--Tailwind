792b6e7c516ed8899fb012524f95b68296de0943
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-05 12:10:50 +0100

    Style button and arrow icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
 const Button = ({ label, iconUrl }) => {
   return (
     <div>
-      <button className="">{label} <img src={iconUrl} alt="" /></button>
+      <button
+        className="flex justify-center items-center gap-2 px-7 py-4 border border-x-coral-red font-montserrat text-lg 
+      leading-none bg-coral-red rounded-full text-white"
+      >
+        {label}{" "}
+        <img
+          src={iconUrl}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      </button>
     </div>
   );
 };
