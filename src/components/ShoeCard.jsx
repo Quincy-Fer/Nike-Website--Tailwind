dd77d2f3cfc807ef7197e22fa4850e963becf6d3
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-05 15:16:10 +0100

    Bugfix re-import jsx files to make it work

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,26 +1,26 @@
-const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImg }) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const handleClick = () => {
-    if (bigShoeImg !== imgUrl.bigShoe) {
-      changeBigShoeImage(imgUrl.bigShoe);
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
     }
   };
+
   return (
     <div
       className={`border-2 rounded-xl ${
-        bigShoeImg === imgUrl.bigShoe
+        bigShoeImg === imgURL.bigShoe
           ? "border-coral-red"
           : "border-transparent"
-      }
-       cursor-pointer max-sm:flex-1`}
+      } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
-      <div className="flex justify-center items-center bg-card bg-center bg-covers sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
         <img
-          src={imgUrl.thumbnail}
-          alt="shoe collection"
+          src={imgURL.thumbnail}
+          alt='shoe colletion'
           width={127}
-          height={107}
-          className="object-contain"
+          height={103.34}
+          className='object-contain'
         />
       </div>
     </div>
