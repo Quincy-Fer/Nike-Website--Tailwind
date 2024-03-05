6e04be0e7d788144f9161d5108d083da9e208be3
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-05 23:26:15 +0100

    Add heading and text dynamically, style

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,6 +4,8 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
       <div className="flex w-11 h-11 justify-center items-center rounded-full bg-coral-red">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
+      <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">{label}</h3>
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">{subtext}</p>
       
     </div>
   );
