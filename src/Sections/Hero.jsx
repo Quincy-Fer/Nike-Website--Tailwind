7f552cc2313264a56bb441484f4e4d897491fa76
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-05 14:53:42 +0100

    Add state and props to Hero, create Shoecard, dynamic border

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -5,6 +5,9 @@ import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
+
+  const [bigShoeImg, setbigShoeImg] = useState(bigShoe1)
+  
   return (
     <section
       id="home"
@@ -41,20 +44,24 @@ const Hero = () => {
         </div>
       </div>
       <div
-        className="relative flex-1 flex justfy-center items-center xl:min-h-screen max-sl:py-40
+        className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40
        bg-primary bg-hero bg-cover bg-center"
       >
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={110}
           className="object-contain relative z-10"
         />
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe, index) => (
             <div key={index}>
-              <ShoeCard />
+              <ShoeCard
+                imgUrl={shoe}
+                changeBigShoeImage={(shoe) => setbigShoeImg(shoe)}
+                bigShoeImage={bigShoeImg}
+              />
             </div>
           ))}
         </div>
