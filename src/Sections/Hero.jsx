fce83205bddb2bc2658bfc7968dde77313cf261f
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-05 11:54:45 +0100

    Create Hero layout left side

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,5 +1,27 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex-col xl:flex-row  justify-center min-h-screen max-container"
+    >
+      <div
+        className="relative xl:w-2/5 flex flex-col justify-center items-start w-full 
+        max-xl:padding-x pt-28"
+      >
+        <p>Our summer collection</p>
+        <h1>
+          <span>The New Arrival </span>
+          <br />
+          <span>Nike </span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
