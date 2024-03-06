cb0b0125a652bff57c5f5ecc0521ea207263fc5a
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-06 15:33:43 +0100

    Write review section headline + paragraph and styling

--- src/Sections/CustomerReviews.jsx
+++ src/Sections/CustomerReviews.jsx
@@ -1,5 +1,12 @@
 const CustomerReviews = () => {
-  return <div>CustomerReviews</div>;
+  return (
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red ">Customers</span> Say
+      </h3>
+      <p className="info-text m-auto mt-4"> Hear genuine stories from our satisfied customers</p>
+    </section>
+  );
 };
 
 export default CustomerReviews;
