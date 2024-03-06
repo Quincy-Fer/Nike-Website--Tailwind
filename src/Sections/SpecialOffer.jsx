f9669e0383d18476f9dab79461971704cf193fbe
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-06 14:00:07 +0100

    Import image for left column style image and layout

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,5 +1,13 @@
+import { offer } from "../assets/images";
+
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className="flex justify-between items-center  max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img src={offer} alt="" width={773} height={687} className="object-contain w-full" />
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
