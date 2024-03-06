921212f78e4be2c62581dd7384f076294c225ff7
Quincy Fer <quincy.e.fer@gmail.com>
2024-03-06 16:24:54 +0100

    Create header subtext and input field

--- src/Sections/Subscribe.jsx
+++ src/Sections/Subscribe.jsx
@@ -1,5 +1,20 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl font-bold leading-[68px] lg:max-w-md font-palanquin ">
+        Sign up for <span className="text-coral-red">Updates</span> & Newsletter
+      </h3>
+      <div className="">
+        <input type="text" placeholder="subscribe@nike.com" className="input"/>
+        <div><Button label="Sign Up" fullWidth /></div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
